5d3cc6738e17bb789a65f7e8631fac1f48005cdc
mengbing <mengbingg@outlook.com>
2023-03-21 17:05:56 +0800

    ..

---
+++ vue/vue3/vue-project/src/components/Child.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: mengbing mengbingg@outlook.com
+ * @Date: 2023-02-16 22:03:54
+ * @LastEditors: mengbing mengbingg@outlook.com
+ * @LastEditTime: 2023-02-16 22:58:54
+ * @Descripttion: 
+-->
+<script setup>
+import { useAttrs } from 'vue'
+
+console.log(useAttrs());
+
+</script>
+
+<template>
+    <div class="greetings">
+        <h3 v-bind="$attrs">
+            You’ve successfully created a project with
+            <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+            <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+        </h3>
+    </div>
+</template>
---
+++ vue/vue3/vue-project/src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import Child from '../components/Child.vue'
+
+// 声明一个 ref 来存放该元素的引用
+// 必须和模板里的 ref 同名
+const input = ref(null)
+
+onMounted(() => {
+  input.value.focus()
+  console.log(input.value)
+});
+const handle = () => {
+  console.log(1111);
+}
+</script>
+
+<template>
+  <input ref="input" />
+  <Child a="你好" b="ya" @cha="handle"></Child>
+</template>
